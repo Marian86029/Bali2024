03cc2a955d96f69e181663bfab999e7f4959676e
Marian86029 <129669608+Marian86029@users.noreply.github.com>
2023-04-28 15:37:52 +0100

    Update main.go

--- main.go
+++ main.go
@@ -1,5 +1,16 @@
 package main
 
-func main() {
+import (
+	"log"
 
+	"github.com/Marian86029/Bali2024/db"
+	"github.com/Marian86029/Bali2024/handlers"
+)
+
+func main() {
+	if db.CheckConnection() == 0 {
+		log.Fatal("Sin Conexion a la base de datos")
+		return
+	}
+	handlers.Manejadores()
 }
